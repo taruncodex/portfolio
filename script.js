7b8f739108c2b825c5afb4002e84fd0771f56cf6
Tarun Rathore <tarunrathore200@gmail.com>
2025-02-07 12:51:50 +0530

    Enhance resume download

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ window.addEventListener('scroll', () => {
 
 //---------------light mode and dark mode---------------------------
 
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-} else {
-    document.documentElement.classList.remove('dark')
-}
+// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+//     document.documentElement.classList.add('dark')
+// } else {
+//     document.documentElement.classList.remove('dark')
+// }
 
 function toggleTheme() {
     document.documentElement.classList.toggle('dark')
